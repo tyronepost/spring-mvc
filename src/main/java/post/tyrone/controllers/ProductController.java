19c3932a5fec89cf54ce115abfe3f6b57b272e89
tyronepost <tyrone.post.a@gmail.com>
2017-04-07 17:35:29 -0400

    adding Products scaffold

---
+++ src/main/java/post/tyrone/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package post.tyrone.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import post.tyrone.services.ProductService;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model) {
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+}
---
+++ src/main/java/post/tyrone/services/ProductService.java
@@ -0,0 +1,9 @@
+package post.tyrone.services;
+
+import post.tyrone.domain.Product;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> listAllProducts();
+}
