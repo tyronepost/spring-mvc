fe6cee7e852a83913b93de04f400ca4272bb5fb5
Tyrone Post <tyrone.post.a@gmail.com>
2017-05-30 11:01:26 -0400

    flushing out Customer assignment

--- src/main/java/post/tyrone/controllers/CustomerController.java
+++ src/main/java/post/tyrone/controllers/CustomerController.java
@@ -1,8 +1,56 @@
 package post.tyrone.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import post.tyrone.domain.Customer;
+import post.tyrone.services.CustomerService;
+
+import java.util.List;
 
 @Controller
 public class CustomerController {
 
+    private CustomerService customerService;
+
+    @Autowired
+    public void setCustomerService(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @RequestMapping("/customers")
+    public String customers(Model model) {
+        List<Customer> customers = customerService.listAllCustomers();
+        model.addAttribute("customers", customers);
+        return "customers/index";
+    }
+
+    @RequestMapping("customer/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        Customer customer = customerService.getCustomerById(id);
+        model.addAttribute("customer", customer);
+        return "customers/show";
+    }
+
+    public String newCustomer() {
+        return "customers/new";
+    }
+
+    public String create() {
+        return "customers/create";
+    }
+
+    public String edit() {
+        return "customers/edit";
+    }
+
+    public String update() {
+        return "customers/update";
+    }
+
+    public String destroy() {
+        return "customers/destroy";
+    }
 }
--- src/main/java/post/tyrone/services/CustomerServiceImpl.java
+++ src/main/java/post/tyrone/services/CustomerServiceImpl.java
@@ -1,18 +1,47 @@
 package post.tyrone.services;
 
+import org.springframework.stereotype.Service;
 import post.tyrone.domain.Customer;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+
+    private Map<Integer, Customer> customers;
+
+    public CustomerServiceImpl() {
+        loadCustomers();
+    }
+
+    private void loadCustomers() {
+        customers = new HashMap<>();
+
+        Customer customer1 = new Customer();
+        customer1.setId(1);
+        customer1.setFirstName("Tyrone");
+        customer1.setLastName("Post");
+        customer1.setAddressLine1("1500 Monterey St");
+        customer1.setCity("Richmond");
+        customer1.setState("California");
+        customer1.setZipCode("94804");
+        customer1.setEmail("tp123@spring.com");
+        customer1.setPhoneNumber("123-456-7890");
+
+        customers.put(1, customer1);
+    }
+
     @Override
     public List<Customer> listAllCustomers() {
-        return null;
+        return new ArrayList<>(customers.values());
     }
 
     @Override
     public Customer getCustomerById(Integer id) {
-        return null;
+        return customers.get(id);
     }
 
     @Override
